2f401cf2b5034352b5f4b7d30a72a3c281f2c718
fengyuanliang3 <fengyuanliang2@jd.com>
2020-10-15 15:53:38 +0800

    feat: svg-icon

--- lib/component/Main.vue
+++ lib/component/Main.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="v-jdesign">
-    <ul class="feature">
+    <ul class="v-jd-feature">
       <a @click="change('fields')">
-        <li :class="{ active: current === 'fields' }">设计</li>
+        <li :class="{ active: current === 'fields' }">
+          <svg-icon name="object-group"></svg-icon>
+          设计
+        </li>
       </a>
       <a @click="change('datasource')">
-        <li :class="{ active: current === 'datasource' }">数据源</li>
+        <li :class="{ active: current === 'datasource' }">
+          <svg-icon name="database"></svg-icon>
+          数据源
+        </li>
       </a>
       <a @click="change('listeners')">
-        <li :class="{ active: current === 'listeners' }">监听</li>
+        <li :class="{ active: current === 'listeners' }">
+          <svg-icon name="podcast"></svg-icon>
+          监听
+        </li>
+      </a>
+      <a @click="change('metadata')">
+        <li :class="{ active: current === 'metadata' }">
+          <svg-icon name="code"></svg-icon>
+          元数据
+        </li>
       </a>
     </ul>
     <div class="content">
       <Fields v-if="current === 'fields'"></Fields>
       <Datasource v-if="current === 'datasource'"></Datasource>
       <Listeners v-if="current === 'listeners'"></Listeners>
+      <Metadata v-if="current === 'metadata'"></Metadata>
     </div>
   </div>
 </template>
@@ -24,6 +40,9 @@ import store from '../store'
 import Fields from './fields/Index'
 import Datasource from './datasource/Index'
 import Listeners from './listeners/Index'
+import Metadata from './metadata/Index'
+import SvgIcon from 'vue-svgicon'
+import '../icons/components'
 
 export default {
   name: 'v-jdesign',
@@ -33,9 +52,11 @@ export default {
     components: Object
   },
   components: {
+    SvgIcon,
     Fields,
     Datasource,
-    Listeners
+    Listeners,
+    Metadata
   },
   data() {
     return {
@@ -54,7 +75,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .v-jdesign {
   font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
     'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
@@ -64,7 +85,18 @@ export default {
   display: flex;
 }
 
-.v-jdesign > ul.feature {
+.v-jdesign .svg-icon {
+  width: 16px;
+  height: 16px;
+  color: inherit;
+}
+
+.v-jdesign .svg-icon.svg-fill {
+  fill: currentColor;
+  stroke: none;
+}
+
+.v-jdesign > ul.v-jd-feature {
   background-color: rgb(48, 65, 86);
   list-style: none;
   margin: 0;
@@ -72,17 +104,21 @@ export default {
   color: rgb(191, 203, 217);
 }
 
-.v-jdesign > ul.feature a {
+.v-jdesign > ul.v-jd-feature a {
   cursor: pointer;
 }
 
-.v-jdesign > ul.feature li {
-  line-height: 56px;
-  padding: 0 15px;
+.v-jdesign > ul.v-jd-feature li {
+  padding: 6px 12px;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 48px;
 }
 
-.v-jdesign > ul.feature li.active {
+.v-jdesign > ul.v-jd-feature li.active {
   color: rgb(64, 158, 255);
   background-color: #263445;
 }
---
+++ lib/component/metadata/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <svg-icon name="code"></svg-icon>
+    元数据
+  </div>
+</template>
+
+<script>
+import SvgIcon from 'vue-svgicon'
+
+export default {
+  components: {
+    SvgIcon
+  }
+}
+</script>
