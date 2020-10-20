4583b2c1f33ca6c61b81c43cadf2ba39db420668
fyl080801 <fyl080801@hotmail.com>
2020-10-21 01:22:24 +0800

    feat: 属性面板

---
+++ lib/components/property/PropertyItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-group">
+    <label>{{ label }}</label>
+    <slot></slot>
+    <small class="form-text text-muted">
+      <slot name="description"></slot>
+    </small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-jd-property-item',
+  props: {
+    label: String
+  }
+}
+</script>
--- lib/views/fields/Property.vue
+++ lib/views/fields/Property.vue
@@ -1,25 +1,82 @@
 <template>
   <div class="property side">
-    <Collapse v-model="actives">
-      <CollapseItem title="分组"></CollapseItem>
+    <Collapse v-if="propForm" v-model="actives">
+      <CollapseItem
+        :name="key"
+        :key="key"
+        v-for="key in Object.keys(propForm)"
+        :title="key"
+      >
+        <v-jform
+          tag="form"
+          class="property-form"
+          :components="components"
+          :fields="propForm[key]"
+          v-model="editing"
+        ></v-jform>
+      </CollapseItem>
     </Collapse>
   </div>
 </template>
 
 <script>
+import { resolveForm } from '../../utils/property'
 import { Collapse, CollapseItem } from '../../components/collapse'
+import { PropertyItem } from '../../components/property'
+import vjform from 'vjform'
+import SvgIcon from 'vue-svgicon'
+import { mapGetters } from 'vuex'
 
 export default {
-  components: { Collapse, CollapseItem },
+  components: { Collapse, CollapseItem, [vjform.name]: vjform },
   data() {
-    return { actives: [] }
+    return {
+      components: { [PropertyItem.name]: PropertyItem, SvgIcon },
+      actives: [],
+      propForm: null
+    }
+  },
+  computed: {
+    ...mapGetters(['profile', 'form', 'edit']),
+    editing() {
+      return this.form.map[this.form.editing]
+    },
+    component() {
+      return this.form.map[this.form.editing]
+        ? this.profile.components.find(
+            item => item.name === this.form.map[this.form.editing].component
+          )
+        : null
+    }
+  },
+  watch: {
+    ['form.editing'](value) {
+      if (value) {
+        this.propForm = resolveForm([
+          'remark',
+          'condition',
+          'fieldOptions.slot',
+          'fieldOptions.class',
+          'fieldOptions.style',
+          ...this.component.properties
+        ])(this.profile.properties, this.edit.registry.editor)
+        this.actives = Object.keys(this.propForm)
+      } else {
+        this.propForm = null
+        this.actives = []
+      }
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .property {
   width: 300px;
   border-left: 1px solid #dcdfe6;
+
+  .property-form {
+    padding: 0.75rem 1.25rem;
+  }
 }
 </style>
