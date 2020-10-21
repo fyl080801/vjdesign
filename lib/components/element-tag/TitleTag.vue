c6f44fec64b370b3aeaa9c0d694764a4ed561b39
fengyuanliang3 <fengyuanliang2@jd.com>
2020-10-21 15:59:45 +0800

    feat: 元数据编辑器

--- lib/components/element-tag/TitleTag.vue
+++ lib/components/element-tag/TitleTag.vue
@@ -6,16 +6,17 @@
 </template>
 
 <script>
-import { isEmpty } from 'lodash-es'
 import SvgIcon from 'vue-svgicon'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'v-jd-title-tag',
-  props: { icon: String, text: String },
+  props: { name: String, text: String },
   components: { SvgIcon },
   computed: {
+    ...mapGetters(['profile']),
     titleIcon() {
-      return isEmpty(this.icon) ? 'square' : this.icon
+      return this.profile.icons[this.name] || 'square'
     }
   }
 }
--- lib/views/metadata/Index.vue
+++ lib/views/metadata/Index.vue
@@ -1,19 +1,112 @@
 <template>
-  <div>
-    <p>
-      <svg-icon name="code"></svg-icon>
-      <span>元数据</span>
-      <svg-icon name="database"></svg-icon>
-    </p>
+  <div class="metadata">
+    <div class="navbar navbar-light bg-light">
+      <div class="form-inline">
+        <button v-if="editing" class="btn btn-light" @click="onFormat">
+          格式
+        </button>
+      </div>
+
+      <div v-if="editing" class="form-inline">
+        <button class="btn btn-light" @click="onSubmit">提交</button>
+        <button class="btn btn-light" @click="onCancel">取消</button>
+      </div>
+      <div v-else>
+        <button class="btn btn-light" @click="onEdit">
+          <SvgIcon name="edit"></SvgIcon>
+          编辑
+        </button>
+      </div>
+    </div>
+    <textarea ref="source"></textarea>
   </div>
 </template>
 
 <script>
+import { CodeMirror } from '../../utils/code'
+import { mapGetters } from 'vuex'
 import SvgIcon from 'vue-svgicon'
 
 export default {
-  components: {
-    SvgIcon
+  components: { SvgIcon },
+  data() {
+    return {
+      code: '',
+      instance: null,
+      editing: false,
+      options: {
+        readOnly: true,
+        theme: 'default',
+        lineNumbers: true,
+        mode: 'application/json',
+        gutters: ['CodeMirror-lint-markers'],
+        lint: true
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['form'])
+  },
+  methods: {
+    // 初始化
+    initialize() {
+      this.code = JSON.stringify(this.form.value)
+      this.instance = CodeMirror.fromTextArea(this.$refs.source, this.options)
+      this.instance.setValue(this.code)
+      this.instance.formatAll()
+      this.instance.on('change', this.onChange)
+    },
+    disponse() {
+      this.instance.off('change', this.onChange)
+      this.instance = null
+    },
+    onChange(code) {
+      this.code = code.getValue()
+    },
+    onFormat() {
+      this.instance.formatAll()
+    },
+    onSubmit() {
+      this.editing = false
+      this.$store.dispatch('form/init', JSON.parse(this.code))
+      this.instance.setOption('readOnly', true)
+      this.instance.formatAll()
+    },
+    onEdit() {
+      this.editing = true
+      this.instance.setOption('readOnly', false)
+    },
+    onCancel() {
+      this.editing = false
+      this.code = JSON.stringify(this.form.value)
+      this.instance.setValue(this.code)
+      this.instance.formatAll()
+      this.instance.setOption('readOnly', true)
+    }
+  },
+  mounted() {
+    this.initialize()
+  },
+  beforeDestroy() {
+    this.disponse()
   }
 }
 </script>
+
+<style lang="scss">
+.v-jdesign {
+  .metadata {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+    > .navbar {
+      border-bottom: 1px solid #dcdfe6;
+    }
+
+    > .CodeMirror {
+      flex: 1;
+    }
+  }
+}
+</style>
