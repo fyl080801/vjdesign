52b2bf492a899f815200fc39cec6036263ae1019
fengyuanliang3 <fengyuanliang2@jd.com>
2020-10-28 21:20:56 +0800

    build: travis

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+# npm run docs:build
+yarn docs:build
+
+# 进入生成的文件夹
+# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
+
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'polish: deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# git push -f git@github.com:AlvinMi/Developer-notes.git master:gh-pages
+git push -f git@github.com:fyl080801/vjdesign.git master:gh-pages
+
+cd -
