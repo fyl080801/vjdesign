3901afcf15fc3c4119a0319d1765de8e94133e33
fengyuanliang3 <fengyuanliang2@jd.com>
2020-10-29 11:12:58 +0800

    build: 自动构建文档

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-# npm run docs:build
 yarn docs:build
 
-# 进入生成的文件夹
-# cd docs/.vuepress/dist
 cd docs/.vuepress/dist
-
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -18,14 +13,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'polish: deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# git push -f git@github.com:AlvinMi/Developer-notes.git master:gh-pages
 git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REF}" master:gh-pages
 
 cd -
