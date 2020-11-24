262ce3b930ad46a8983f74573801701fe4e6bdcf
fengyuanliang3 <fengyuanliang2@jd.com>
2020-11-24 13:35:37 +0800

    feat: 属性值支持选择类型

--- lib/components/property/PropertyItem.vue
+++ lib/components/property/PropertyItem.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="form-group">
+  <div :class="{ 'form-group': true, transform: isTransform }">
     <label>{{ label }}</label>
+    <div class="propterty-type">
+      <a href="javascript:;" @click="onChangeType">
+        <span
+          class="badge badge-pill"
+          :class="!isTransform ? 'badge-primary' : ''"
+          >值</span
+        >
+      </a>
+      <a href="javascript:;" @click="onChangeType">
+        <span
+          class="badge badge-pill"
+          :class="isTransform ? 'badge-primary' : ''"
+          >转换</span
+        >
+      </a>
+    </div>
     <div class="property-tail">
       <a href="javascript:;" v-if="hasValue" @click="onClear">清空</a>
     </div>
-    <slot></slot>
+    <slot v-if="!isTransform"></slot>
+
     <small class="form-text text-muted">
       <slot name="description"></slot>
     </small>
@@ -22,11 +39,21 @@ export default {
   computed: {
     hasValue() {
       return this.value !== undefined
+    },
+    isTransform() {
+      return (
+        typeof this.value === 'object' &&
+        this.value !== null &&
+        this.value.$type === 'design'
+      )
     }
   },
   methods: {
     onClear() {
       this.$emit('clear', this.prop)
+    },
+    onChangeType() {
+      this.$emit('changeType', !this.isTransform)
     }
   }
 }
@@ -35,5 +62,15 @@ export default {
 <style lang="scss" scoped>
 .property-tail {
   float: right;
+}
+
+.transform {
+}
+
+.form-group {
+  .propterty-type {
+    display: inline-block;
+    margin-left: 1rem;
+  }
 }
 </style>
