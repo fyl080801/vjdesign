e0792c23753280fb7fdbe6e4b4bda00c1b8f2d5e
fengyuanliang3 <fengyuanliang2@jd.com>
2020-12-16 11:27:33 +0800

    feat: 数组编辑器更新

--- lib/components/controls/ArrayProperty.vue
+++ lib/components/controls/ArrayProperty.vue
@@ -2,18 +2,13 @@
   <div class="list-group list-group-flush">
     <div class="list-group-item" :key="index" v-for="(item, index) in value">
       <div>
-        <v-jform
-          :value="item"
-          :fields="fields"
-          :datasource="datasource"
-          :listeners="listeners"
-        ></v-jform>
-        <button type="button" class="btn btn-sm btn-link" @click="editItem">
+        <span></span>
+        <a @click="editItem(index)">
           编辑
-        </button>
-        <button type="button" class="btn btn-sm btn-link" @click="removeItem">
+        </a>
+        <a @click="removeItem(index)">
           删除
-        </button>
+        </a>
       </div>
     </div>
     <a
@@ -30,6 +25,7 @@
 import SvgIcon from 'vue-svgicon'
 import { mapGetters } from 'vuex'
 import { resolveProperties } from '../../utils/property'
+import { cloneDeep } from 'lodash-es'
 
 export default {
   components: { SvgIcon },
@@ -107,12 +103,35 @@ export default {
         }
       })
     },
-    editItem() {},
-    removeItem() {
-      this.$store.dispatch('popup/confirm', {
-        title: '删除',
-        content: '是否删除？'
+    editItem(index) {
+      const model = { data: cloneDeep(this.value[index]) }
+
+      this.$store.dispatch('popup/show', {
+        model,
+        form: {
+          initialling: ({ functional }) => {
+            functional('SUBMIT_OBJECT', () => {
+              const list = [...(this.value || [])]
+              list[index] = model.data
+              this.$emit('input', list)
+              this.$store.dispatch('popup/close')
+            })
+          },
+          fields: this.resolveForm('添加项')
+        }
       })
+    },
+    removeItem(index) {
+      this.$store
+        .dispatch('popup/confirm', {
+          title: '删除',
+          content: '是否删除？'
+        })
+        .then(() => {
+          const list = [...this.value]
+          list.splice(index, 1)
+          this.$emit('input', list)
+        })
     }
   }
 }
